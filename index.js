29c4fce9027740d548ca27f6f93b7e6e2213cb90
Potor <liupotor4@gmail.com>
2022-04-06 20:16:21 -0700

    test workflow

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const loadGameData = require('./scripts/loadGameData');
 const trackRankingRate = require('./scripts/trackRankingRate');
 
 loadGameData(0, async () => {
-  trackRankingRate()
+  // trackRankingRate()
 });
